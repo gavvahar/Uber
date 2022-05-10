68407eb6e8991e2dd73f28072d1655d8e3a28e87
Nihar Gavva <nihargavva@gmail.com>
2022-05-10 16:52:48 +0000

    going to be working on it on vscode today

---
+++ script.js
@@ -0,0 +1,15 @@
+const addToCartArr = [
+  document.querySelector('#add-to-cart1'),
+  document.querySelector('#add-to-cart2'),
+  document.querySelector('#add-to-cart3'),
+  document.querySelector('#add-to-cart4'),
+  document.querySelector('#add-to-cart5'),
+  document.querySelector('#add-to-cart6'),
+  document.querySelector('#add-to-cart7'),
+  document.querySelector('#add-to-cart8'),
+  document.querySelector('#add-to-cart9')
+]
+function cart()
+  {
+    
+  }
